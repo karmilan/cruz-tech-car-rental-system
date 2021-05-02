673dc7448c6ab8fa5289c728c0d7e1b204b36bae
Your Name <delftkarmilan@gmail.com>
2021-05-02 23:19:37 +0530

    commit_4_database_updated

--- carrental.sql
+++ carrental.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 01, 2021 at 06:48 AM
+-- Generation Time: May 02, 2021 at 07:48 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.0
 
@@ -36,8 +36,11 @@ CREATE TABLE `booking` (
   `car_model` varchar(50) NOT NULL,
   `car_seatingcapacity` varchar(50) NOT NULL,
   `cust_drivinglicenseno` int(20) NOT NULL,
-  `car_dailycharge` int(11) NOT NULL,
-  `bookingdays` int(11) NOT NULL,
+  `car_dailycharge` int(11) DEFAULT NULL,
+  `car_monthlycharge` int(50) DEFAULT NULL,
+  `bookingdate` datetime DEFAULT NULL,
+  `actual_returndate` datetime DEFAULT NULL,
+  `car_hourlycharge` int(50) DEFAULT NULL,
   `amount` int(11) NOT NULL,
   `bookingtime` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -46,9 +49,43 @@ CREATE TABLE `booking` (
 -- Dumping data for table `booking`
 --
 
-INSERT INTO `booking` (`bookingid`, `car_image`, `car_name`, `car_category`, `car_brand`, `car_model`, `car_seatingcapacity`, `cust_drivinglicenseno`, `car_dailycharge`, `bookingdays`, `amount`, `bookingtime`) VALUES
-(18, '', 'bb', 'Luxury', 'bmw', 'ee', '4', 11111, 0, 5, 0, '2021-04-30 22:00:03'),
-(19, '', 'bb', 'Luxury', 'bmw', 'ee', '4', 11111, 0, 5, 0, '2021-04-30 22:04:32');
+INSERT INTO `booking` (`bookingid`, `car_image`, `car_name`, `car_category`, `car_brand`, `car_model`, `car_seatingcapacity`, `cust_drivinglicenseno`, `car_dailycharge`, `car_monthlycharge`, `bookingdate`, `actual_returndate`, `car_hourlycharge`, `amount`, `bookingtime`) VALUES
+(1, '', 'dcv', 'Luxury', '', '', '0', 11, 0, 1000, '2021-05-02 14:08:00', '2021-07-02 14:08:00', 0, 0, '2021-05-02 14:08:20'),
+(2, '', 'dcv', 'Luxury', '', '', '0', 11, 0, 1000, '2021-05-02 14:08:00', '2021-07-02 14:08:00', 0, 0, '2021-05-02 14:08:50'),
+(3, '', 'dcv', 'Luxury', '', '', '0', 11, 0, 1000, '2021-05-02 14:08:00', '2021-07-02 14:08:00', 0, 0, '2021-05-02 14:09:18'),
+(4, '', 'dcv', 'Luxury', '', '', '0', 11, 0, 1000, '2021-05-02 14:49:00', '2021-07-02 14:49:00', 0, 0, '2021-05-02 14:49:12'),
+(5, '', 'dcv', 'Luxury', '', '', '0', 11, 0, 1000, '2021-05-02 14:49:00', '2021-07-02 14:49:00', 0, 0, '2021-05-02 14:52:11'),
+(6, '', 'dcv', 'Luxury', '', '', '0', 0, 0, 1000, '2021-05-02 14:52:00', '2021-07-02 14:52:00', 0, 0, '2021-05-02 14:52:44'),
+(7, '', 'dcv', 'Luxury', '', '', '0', 11, 0, 1000, '2021-05-02 14:54:00', '2021-07-02 14:54:00', 0, 0, '2021-05-02 14:54:36'),
+(8, '', 'dcv', 'Luxury', '', '', '0', 11, 0, 1000, '2021-05-02 14:57:00', '2021-07-02 14:57:00', 0, 0, '2021-05-02 14:57:43'),
+(9, '', 'dcv', 'Luxury', '', '', '0', 11, 0, 1000, '2021-05-02 17:55:00', '2021-07-02 17:55:00', 0, 2000, '2021-05-02 17:56:05'),
+(10, '', 'dcv', 'Luxury', '', '', '0', 11, 0, 1000, '2021-05-02 17:58:00', '2021-08-04 19:58:00', 0, 3000, '2021-05-02 17:58:40'),
+(11, '', 'dcv', 'Luxury', '', '', '0', 11, 0, 1000, '2021-05-02 18:00:00', '2021-09-02 18:00:00', 0, 4000, '2021-05-02 18:00:44'),
+(12, '', 'dcv', 'Luxury', '', '', '0', 11, 0, 1000, '2021-05-02 18:02:00', '2021-08-02 18:02:00', 0, 3000, '2021-05-02 18:02:15'),
+(13, '', 'dcv', 'Luxury', '', '', '0', 11, 0, 1000, '2021-05-02 18:04:00', '2021-07-02 18:04:00', 0, 2000, '2021-05-02 18:05:02'),
+(14, '', 'dcv', 'Luxury', '', '', '0', 0, 100, 1000, '2021-05-02 18:11:00', '2021-07-03 18:11:00', 0, 1, '2021-05-02 18:11:53'),
+(15, '', 'dcv', 'Luxury', '', '', '0', 11, 100, 1000, '2021-05-02 18:17:00', '2021-07-03 18:17:00', 0, 0, '2021-05-02 18:18:00'),
+(16, '', 'dcv', 'Luxury', '', '', '0', 11, 100, 1000, '2021-05-02 18:19:00', '2021-07-03 18:19:00', 0, 0, '2021-05-02 18:20:00'),
+(17, '', 'dcv', 'Luxury', '', '', '0', 33, 100, 1000, '2021-05-02 18:20:00', '2021-07-03 18:20:00', 0, 0, '2021-05-02 18:20:57'),
+(18, '', 'dcv', 'Luxury', '', '', '0', 11, 100, 1000, '2021-05-02 18:21:00', '2021-07-03 18:22:00', 0, 0, '2021-05-02 18:22:12'),
+(19, '', 'dcv', 'Luxury', '', '', '0', 11, 100, 1000, '2021-05-02 18:24:00', '2021-07-03 18:24:00', 0, 0, '2021-05-02 18:24:54'),
+(20, '', 'dcv', 'Luxury', '', '', '0', 33, 100, 1000, '2021-05-02 18:28:00', '2021-07-03 18:28:00', 0, 0, '2021-05-02 18:28:22'),
+(21, '', 'dcv', 'Luxury', '', '', '0', 11, 100, 1000, '2021-05-02 18:33:00', '2021-07-03 18:33:00', 0, 0, '2021-05-02 18:33:25'),
+(22, '', 'dcv', 'Luxury', '', '', '0', 0, 100, 1000, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 2100, '2021-05-02 18:33:45'),
+(23, '', 'dcv', 'Luxury', '', '', '0', 11, 100, 1000, '2021-05-02 18:34:00', '2021-07-03 18:35:00', 0, 0, '2021-05-02 18:35:10'),
+(24, '', 'dcv', 'Luxury', '', '', '0', 11, 100, 1000, '2021-05-02 18:46:00', '2021-07-03 18:47:00', 0, 0, '2021-05-02 18:47:09'),
+(25, '', 'dcv', 'Luxury', '', '', '0', 11, 100, 1000, '2021-05-02 18:50:00', '2021-07-03 18:51:00', 0, 0, '2021-05-02 18:51:06'),
+(26, '', 'dcv', 'Luxury', '', '', '0', 11, 100, 1000, '2021-05-02 18:53:00', '2021-07-03 18:53:00', 0, 0, '2021-05-02 18:53:56'),
+(27, '', 'dcv', 'Luxury', '', '', '0', 11, 100, 1000, '2021-05-02 19:11:00', '2021-07-03 19:11:00', 0, 0, '2021-05-02 19:12:00'),
+(28, '', 'dcv', 'Luxury', '', '', '0', 11, 100, 1000, '2021-05-02 19:13:00', '2021-07-03 19:13:00', 0, 0, '2021-05-02 19:13:23'),
+(29, '', 'dcv', 'Luxury', '', '', '0', 11, 100, 1000, '2021-05-02 19:14:00', '2021-07-03 19:14:00', 0, 0, '2021-05-02 19:14:27'),
+(30, '', 'dcv', 'Luxury', '', '', '0', 11, 100, 1000, '2021-05-02 19:23:00', '2021-07-03 19:24:00', 0, 2100, '2021-05-02 19:25:50'),
+(31, '', 'dcv', 'Luxury', '', '', '0', 11, 100, 1000, '2021-05-02 20:03:00', '2021-07-03 21:03:00', 0, 2110, '2021-05-02 20:03:44'),
+(32, '', 'dcv', 'Luxury', '', '', '0', 11, 100, 1000, '2021-05-02 20:15:00', '2021-05-02 21:59:00', 10, 10, '2021-05-02 20:16:02'),
+(33, '', 'dcv', 'Luxury', '', '', '0', 0, 100, 1000, '2021-05-02 20:28:00', '2021-06-03 21:28:00', 10, 1110, '2021-05-02 20:28:21'),
+(34, '', 'dcv', 'Luxury', '', '', '0', 11, 100, 1000, '2021-05-02 20:42:00', '2021-06-03 21:47:00', 10, 1110, '2021-05-02 20:43:12'),
+(35, '', 'Lol', 'Luxury', 'Benz', 'a-1', '4', 33, 10000, 25000, '2021-05-02 21:53:00', '2021-06-03 21:53:00', 1000, 35000, '2021-05-02 20:54:06'),
+(36, '', 'Lol', 'Luxury', 'Benz', 'a-1', '4', 22, 10000, 25000, '2021-05-02 20:55:00', '2021-06-03 21:57:00', 1000, 36000, '2021-05-02 20:55:59');
 
 -- --------------------------------------------------------
 
@@ -87,7 +124,8 @@ CREATE TABLE `car` (
 INSERT INTO `car` (`carid`, `category`, `brand`, `model`, `carname`, `type`, `seatingcapacity`, `plateno`, `fueltype`, `fuelcapacity`, `manufactureyr`, `colour`, `hourlycharge`, `dailycharge`, `weeklycharge`, `monthlycharge`, `insurancecharge`, `details`, `branch`, `image`, `available`) VALUES
 (12, 'Luxury', 'bmw', 'ee', 'bb', 'Sedan', 4, '444', 'Petrol', 5, 2021, 'hh', 7, 0, 0, 0, 0, '', '', '', 'yes'),
 (13, 'Luxury', 'bmw', 'ee', 'bb', 'Sedan', 4, '444', 'Petrol', 5, 2021, 'hh', 7, 0, 0, 0, 0, '', '', '', 'yes'),
-(19, 'Luxury', '', '', 'dcv', 'Sedan', 0, '', '', 0, 2021, '', 0, 0, 0, 0, 0, '', '', '', 'yes');
+(19, 'Luxury', '', '', 'dcv', 'Sedan', 0, '', '', 0, 2021, '', 10, 100, 0, 1000, 0, '', '', '', 'yes'),
+(20, 'Luxury', 'Benz', 'a-1', 'Lol', 'Hatchback', 4, '223', 'Petrol', 5, 2001, 'Black', 1000, 10000, 5000, 25000, 800, 'erg', 'col branch', 'Ajith_Kumar_thala58_2.jpg', 'yes');
 
 -- --------------------------------------------------------
 
@@ -207,9 +245,7 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`userid`, `fullname`, `email`, `phno`, `username`, `pass`) VALUES
 (2, 'karmilan', 'karm@gmail.com', 773322976, 'admin', 'admin'),
-(5, 'supa', 'supa@gmail.com', 778866555, 'su', 'pa'),
-(6, 'supa', 'supa@gmail.com', 778866555, 'su', 'pa'),
-(7, 'supa', 'supa@gmail.com', 778866555, 'su', 'pa');
+(8, 'banu', 'bb', 999, 'ban', 'ban');
 
 --
 -- Indexes for dumped tables
@@ -265,13 +301,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `booking`
 --
 ALTER TABLE `booking`
-  MODIFY `bookingid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `bookingid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `car`
 --
 ALTER TABLE `car`
-  MODIFY `carid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `carid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `customer`
@@ -301,7 +337,7 @@ ALTER TABLE `mechanic`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `userid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `userid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
